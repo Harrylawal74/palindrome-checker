2880b5a93ff6bce010417645f587924fb80ae166
Harry Lawal <harrylawal74@gmail.com>
2024-09-06 15:11:35 +0100

    Moving to Github
    moving my projects to github

---
+++ checker.js
@@ -0,0 +1,31 @@
+const input = document.getElementById("input")
+
+
+
+//takes string and splits it into array letter by letter
+//reverses array
+//joins letters back together
+function reversestring(str) {
+    return str.split("").reverse().join("")
+    
+}
+
+
+//compares the input value against the reversed value 
+//if identical then input is a palindrome
+//.toLowerCase function to fix capitalisation problem
+function check() {
+    const value = input.value.toLowerCase();
+    const reverse = reversestring(value)
+    
+    if (value == reverse) {
+        alert("Palindrome")
+    }
+    else {
+        alert("Not Palindrome")
+    }
+
+    //removes string from box at end of check() run
+    input.value = ""
+
+}
